0bcba5c5329db8ab5fb2c9eba53d0a18423ae19a
Matthew Thrasher <matthewth@marvin.com>
2017-03-02 12:45:05 -0600

    Add campaign finance tab

--- routes/candidate.js
+++ routes/candidate.js
@@ -16,8 +16,8 @@ router.get('/:idCandidates_To_Race', function (req, res) {
   //get everything from the candidate table, pertinent race results, and whether or not they have financial information (if there is a committee listed under their name)
     dataLayer.getCandidate_SplashInfo(req.params.idCandidates_To_Race, function(err,rows,fields){
     	if(!err){
-    		console.log(rows[0][0]);
-    		res.render('candidate', {title: 'Candidate Page', pageTitle: rows[0][0].First + ' ' + rows[0][0].Last, candidateInfo: rows[0][0]});
+    		console.log(rows[3]);
+    		res.render('candidate', {title: 'Candidate Page', pageTitle: rows[0][0].First + ' ' + rows[0][0].Last, candidateInfo: rows[0][0], mayorRaceInfo: rows[2], wardRaceInfo: rows[1], hasFinancialCommittee: rows[3]});
     	} else {
     		console.log('Error while performing Query: ' + err);
     	}
